53125ea70f4e4f3bb53985a5226fed0b826f5145
marsh <marshalshuai@aliyun.com>
2020-09-26 15:37:44 +0800

    first commit

--- src/main/java/com/mars/phms/repository/RoleRepository.java
+++
@@ -1,9 +0,0 @@
-package com.mars.phms.repository;
-
-import com.mars.phms.domain.PhRole;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface RoleRepository extends JpaRepository<PhRole,Long>{
-    PhRole findByAuthority(String authority);
-}
